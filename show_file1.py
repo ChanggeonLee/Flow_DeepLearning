676c6f7cab0408fa5230bb11bd659aa2d39978cf
ChanggunLee <cd8114@gmail.com>
2018-10-08 11:04:57 +0900

    delete

--- show_file1.py
+++ show_file1.py
@@ -4,11 +4,10 @@ import numpy as np
 
 shape = [128, 256]
 # open the file
-with h5py.File("fluid_flow_0002.h5") as dataH5:
+with h5py.File("./fluid_flow_0002.h5") as dataH5:
   # get the keys
   key_list = dataH5.keys()
-  print(key_list)
-
+  
   flow = np.array(dataH5['Velocity_0'][:])
   flow = flow.reshape([shape[0], shape[1]+128, 3])[0:shape[0],0:shape[1],0:2]
 
@@ -19,6 +18,8 @@ with h5py.File("fluid_flow_0002.h5") as dataH5:
   boundary_concat = np.concatenate(3*[boundary], axis=2)
   sflow_plot = np.sqrt(np.square(sflow_plot[:,:,0])) - .05 *boundary_concat[:,:,0]
 
+  printf(flow.shape)
+  
   plt.imshow(sflow_plot)
   plt.colorbar()
   plt.show()
